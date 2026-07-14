a2a93c20027d80ff9085859b6f4f431f6fb75f55
danglhse170518-collab <danglhse170518@fpt.edu.vn>
2026-07-14 12:12:07 +0700

    chore: remove chat realtime audit files for re-attribution

--- src/EduAI.BusinessLogic/Services/NullSubjectNotificationService.cs
+++
@@ -1,9 +0,0 @@
-using EduAI.BusinessLogic.IService;
-using EduAI.Model.DTOs;
-
-namespace EduAI.BusinessLogic.Services;
-
-public class NullSubjectNotificationService : ISubjectNotificationService
-{
-    public Task NotifySubjectChangedAsync(SubjectRealtimeEventDto evt) => Task.CompletedTask;
-}
